edd09c3fe4619c058592c8b34c9148b6c206500b
mospective <mrh@codehousegroup.com>
2023-01-16 13:13:01 +0000

    next/image - testing different images internal and external

--- app/contact/person/page.js
+++ app/contact/person/page.js
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import testImage from "../../../public/vercel.svg";
 import '../../../styles/globals.css';
 
 export default function Page() {
-    return(<h1>Person page</h1>)
+    return (<>
+        <h1>Person page</h1>
+        <Image
+            src={testImage}
+            alt="Vercel logo"
+            // width={500} automatically provided
+            // height={500} automatically provided
+            // blurDataURL="data:..." automatically provided
+            // placeholder="blur" // Optional blur-up while loading
+        />
+
+        <Image
+            src="https://miro.medium.com/max/1400/1*pJdLvOAPgVTfESZlSiCTwQ.png"
+            alt="Vercel logo"
+            width={1400}
+            height={600}
+            // blurDataURL="data:..." automatically provided
+            // placeholder="blur" // Optional blur-up while loading
+        />
+
+    </>
+    )
 }
